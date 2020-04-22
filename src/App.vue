58afde821916a2eadcbb1aece39748471d70e909
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-22 21:02:35 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template lang="html">
+  <div id="app">
+      <div class="container d-flex justify-content-center align-items-center">
+          <router-view></router-view>
+      </div>
+  </div>
+</template>
