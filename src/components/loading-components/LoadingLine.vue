3b735281bcbd00f929476887fb229496bad75aa2
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-23 10:54:46 +0200

    Create LoadingLine.vue

---
+++ src/components/loading-components/LoadingLine.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div :class="['LoadingLine grow--animation rounded-full bg-grey', width ? width : 'w-16', height ? height : 'h-4']" />
+</template>
+
+<script>
+export default {
+  name: "LoadingLine",
+  props: {
+    height: {
+      default: "",
+      type: String
+    },
+    width: {
+      default: "",
+      type: String
+    }
+  }
+};
+</script>
