f274595ad1d6c20117a4c14e5356a4633868252d
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-23 22:59:07 +0200

    Update LoadingLine.vue

--- src/components/loading-components/LoadingLine.vue
+++ src/components/loading-components/LoadingLine.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div :class="['LoadingLine grow--animation rounded-full bg-grey', width ? width : 'w-16', height ? height : 'h-4']" />
+  <div :class="['LoadingLine grow--animation bg-grey', width ? width : 'w-16', height ? height : 'h-4']" />
 </template>
 
 <script>
