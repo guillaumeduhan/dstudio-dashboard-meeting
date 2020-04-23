e21091281f042ace24ad4306f89fa44ac81d57c4
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-23 10:54:48 +0200

    Create LoadingThumbnail.vue

---
+++ src/components/loading-components/LoadingThumbnail.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="LoadingThumbnail grow--animation h-16 w-16 rounded-full bg-grey" />
+</template>
+
+<script>
+export default {
+  name: "LoadingThumbnail",
+  props: {
+    imageUrl: {
+      default: "",
+      type: String
+    }
+  }
+};
+</script>
