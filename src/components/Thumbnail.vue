4b5c350666242bd5c229f2d75a13aba5b1f8c667
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-23 10:55:04 +0200

    Create Thumbnail.vue

---
+++ src/components/Thumbnail.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="Thumbnail h-24 w-24" />
+</template>
+
+<script>
+export default {
+  name: "Thumbnail",
+  props: {
+    imageUrl: {
+      default: "",
+      type: String
+    }
+  }
+};
+</script>
