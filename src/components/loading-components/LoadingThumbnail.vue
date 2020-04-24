daabdc933eb4e9a09a124571e7394307cb408758
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-24 22:20:04 +0200

    Update LoadingThumbnail.vue

--- src/components/loading-components/LoadingThumbnail.vue
+++ src/components/loading-components/LoadingThumbnail.vue
@@ -1,15 +1,24 @@
 <template lang="html">
-  <div class="LoadingThumbnail grow--animation h-16 w-16 rounded-full bg-grey" />
+  <div
+    :class="[
+      'LoadingThumbnail grow--animation rounded-full bg-grey',
+      width ? width : 'h-16 w-16',
+    ]"
+  />
 </template>
 
 <script>
 export default {
   name: "LoadingThumbnail",
   props: {
-    imageUrl: {
+    height: {
       default: "",
-      type: String
-    }
-  }
+      type: String,
+    },
+    width: {
+      default: "",
+      type: String,
+    },
+  },
 };
 </script>
