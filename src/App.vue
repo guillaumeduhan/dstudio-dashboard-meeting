da526350ba3fa6717d3a8cb0a4ed5cb135428135
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-24 22:20:01 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,26 @@
     <div class="w-4/6">
       <LoadingDashboard v-if="isLoading" />
     </div>
-    <div class="w-2/6 h-screen bg-white"></div>
+    <div class="w-2/6 h-screen bg-white">
+      <LoadingSideBar v-if="isLoading" />
+    </div>
   </div>
 </template>
 
 <script>
 import LoadingDashboard from "./views/LoadingDashboard.vue";
+import LoadingSideBar from "./views/LoadingSidebar.vue";
 
 export default {
   name: "App",
   components: {
-    LoadingDashboard
+    LoadingDashboard,
+    LoadingSideBar,
   },
   data() {
     return {
-      isLoading: true
+      isLoading: true,
     };
-  }
+  },
 };
 </script>
