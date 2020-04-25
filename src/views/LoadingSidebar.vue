9aad120e71d7550f1fa8936095f58e90ee3d6c50
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-25 14:22:56 +0200

    Update LoadingSidebar.vue

--- src/views/LoadingSidebar.vue
+++ src/views/LoadingSidebar.vue
@@ -1,6 +1,6 @@
-<template lang="html">
+<template>
   <div id="LoadingSidebar" class="flex flex-col justify-between h-screen p-12">
-    <header class="flex justify-between mb-8">
+    <header class="flex justify-between mb-24">
       <div class="w-1/2 flex">
         <LoadingThumbnail width="h-8 w-8 mr-4" />
         <LoadingThumbnail width="h-8 w-8" />
@@ -13,6 +13,21 @@
         <LoadingThumbnail />
       </div>
     </header>
+    <div class="Calendar flex flex-col">
+      <div class="w-full flex">
+        <div class="w-1/2 flex justify-between">
+          <LoadingLine class="w-40 h-8 mb-4 rounded-big" />
+        </div>
+        <div class="w-1/2 flex justify-end">
+          <LoadingThumbnail width="h-8 w-8 mr-4" />
+          <LoadingThumbnail width="h-8 w-8" />
+        </div>
+      </div>
+      <LoadingLine class="w-full h-56 mb-4 rounded-big" />
+    </div>
+    <div class="w-full h-full">
+      <LoadingLine class="w-full h-full mb-4 rounded-big" />
+    </div>
   </div>
 </template>
 
